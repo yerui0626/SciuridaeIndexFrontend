50ec950347e0f9ee9668cedd6b17da6822b8ee48
“yerui0626” <yerui0626@126.com>
2022-05-26 16:21:39 +0800

    2022.5.26更新登录组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import SignIn from './components/SignIn.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <SignIn/>
 </template>
 
-<style>
+<!-- <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,4 +17,4 @@ import HelloWorld from './components/HelloWorld.vue'
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
